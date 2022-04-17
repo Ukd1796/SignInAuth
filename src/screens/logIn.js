88013e249c35128ce2c18c7a123c12e14ff71ec4
Ujjwal <ukdwiwedi@1796>
2022-04-17 13:41:22 +0530

    facebook auth commit

--- src/screens/logIn.js
+++ src/screens/logIn.js
@@ -1,7 +1,8 @@
-import React from "react";
-import {View,Text,StyleSheet,TextInput,Button,Image,TouchableOpacity} from 'react-native';
+import React,{useState} from "react";
+import {View,Text,StyleSheet,TextInput,Button,Image,TouchableOpacity,Pressable} from 'react-native';
 import MaterialIcons from "@expo/vector-icons/MaterialIcons"
 import AntDesign from "@expo/vector-icons/AntDesign"
+import * as Facebook from 'expo-facebook';
 const styles = StyleSheet.create({
 
     upperContainer:{
@@ -35,18 +36,40 @@ const styles = StyleSheet.create({
         marginLeft:160,
         marginTop:-75,
     }
-
 })
 
 
 const LogIn = ()=>{
+
+    
+    async function facebookLogIn() {
+        try {
+          await Facebook.initializeAsync({
+            appId: '1917027175149292',
+          });
+          const { type, token, expirationDate, permissions, declinedPermissions } =
+            await Facebook.logInWithReadPermissionsAsync({
+              permissions: ['public_profile'],
+            });
+          if (type === 'success') {
+            // Get the user's name using Facebook's Graph API
+            const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+            Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+          } else {
+            // type === 'cancel'
+          }
+        } catch ({ message }) {
+          alert(`Facebook Login Error: ${message}`);
+        }
+      }
+
    return( <View style= {styles.upperContainer}>
    <Text style = {styles.signInText}>Sign-In Through</Text>
-   <MaterialIcons name="facebook" size={80} style={styles.facebookIcon}></MaterialIcons>
-   <AntDesign name="google" size={70} style={styles.googleIcon}></AntDesign>
-   <AntDesign name="github" size ={70} style={styles.githubIcon}></AntDesign>
+   <Pressable><MaterialIcons onPress={facebookLogIn} name="facebook" size={80} style={styles.facebookIcon}></MaterialIcons></Pressable>
+   <Pressable><AntDesign name="google" size={70} style={styles.googleIcon}></AntDesign></Pressable>
+   <Pressable><AntDesign name="github" size ={70} style={styles.githubIcon}></AntDesign></Pressable>
     </View>
    );
-}
+   }
 
 export default LogIn;
